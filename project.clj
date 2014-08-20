f29a39060ef9241e96fbf0ceb08041d0042623cd
Paul Gearon <pgearon@revelytix.com>
2014-08-19 23:24:32 -0400

    Added project file to build/test

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject org.clojars.quoll/data.xml "0.0.8-SNAPSHOT"
+  :description "data.xml is a Clojure library for reading and writing XML data. This library is the successor to lazy-xml. This is a private fork that includes namespaces."
+  :url "https://github.com/quoll/data.xml"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  
+  :source-paths ["src/main/clojure"]
+  :test-paths ["src/test/clojure"])
